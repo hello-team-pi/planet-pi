76a14eaaee14005592f5c969a482a3a83f37d9f3
Léon Baudouin <leondedouin@gmail.com>
2021-11-29 21:50:33 +0100

    :construction: Added webgl structure

---
+++ src/webgl/Background/index.ts
@@ -0,0 +1,47 @@
+import fragmentShader from "./index.frag?raw";
+import vertexShader from "./index.vert?raw";
+import * as THREE from "three";
+import AbstractObject from "../abstract/AbstractObject";
+import { MainSceneContext } from "../Scenes/MainScene";
+
+export default class Background extends AbstractObject<MainSceneContext> {
+  private params = {
+    insideColor: "#373232",
+    outsideColor: "#141414",
+    gradientStart: 0,
+    gradientEnd: 0.8,
+  };
+
+  private uniforms: Record<string, THREE.IUniform>;
+
+  public mesh: THREE.Mesh;
+
+  constructor(context: MainSceneContext) {
+    super(context);
+    this.setupMesh(this.context.camera);
+  }
+
+  private setupMesh(camera: THREE.Camera) {
+    this.uniforms = {
+      uInsideColor: { value: new THREE.Color(this.params.insideColor) },
+      uOutsideColor: { value: new THREE.Color(this.params.outsideColor) },
+      uGradientStart: { value: this.params.gradientStart },
+      uGradientEnd: { value: this.params.gradientEnd },
+      uCameraPosition: { value: camera.position },
+      uScreenResolution: {
+        value: new THREE.Vector2(window.innerWidth, window.innerHeight),
+      },
+    };
+
+    this.output = new THREE.Mesh(
+      new THREE.PlaneBufferGeometry(100, 100),
+      new THREE.RawShaderMaterial({
+        fragmentShader: fragmentShader,
+        vertexShader: vertexShader,
+        depthTest: false,
+        uniforms: this.uniforms,
+      })
+    );
+    this.output.renderOrder = -1;
+  }
+}
---
+++ src/webgl/Scenes/MainScene/index.ts
@@ -0,0 +1,60 @@
+import * as THREE from "three";
+import { WebGLAppContext } from "../..";
+import AbstractObject from "../../abstract/AbstractObject";
+import AbstractObjectWithSize from "../../abstract/AbstractObjectWithSize";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import Background from "../../Background";
+
+export type MainSceneContext = WebGLAppContext & {
+  scene: THREE.Scene;
+  camera: THREE.PerspectiveCamera;
+};
+export default class MainScene extends AbstractObjectWithSize {
+  public scene: THREE.Scene;
+  public camera: THREE.PerspectiveCamera;
+
+  private tickingObjects: AbstractObject[] = [];
+
+  constructor(context: WebGLAppContext) {
+    super(context);
+    this.setCamera();
+    this.setObjects();
+    this.context.renderer.compile(this.scene, this.camera);
+  }
+
+  private genContext = (): MainSceneContext => ({
+    ...this.context,
+    camera: this.camera,
+    scene: this.scene,
+  });
+
+  protected onResize(width: number, height: number) {
+    this.camera.aspect = width / height;
+    this.camera.updateProjectionMatrix();
+  }
+
+  private setCamera() {
+    this.camera = new THREE.PerspectiveCamera(
+      22.9,
+      window.innerWidth / window.innerHeight,
+      0.01,
+      1000
+    );
+    this.camera.position.z = 10;
+    this.onResize(window.innerWidth, window.innerHeight);
+    new OrbitControls(this.camera, this.context.renderer.domElement);
+  }
+
+  private setObjects() {
+    this.scene = new THREE.Scene();
+    this.scene.background = new THREE.Color(0x000000);
+    const background = new Background(this.genContext());
+    this.scene.add(background.output);
+  }
+
+  public tick(...params: Parameters<AbstractObject["tick"]>) {
+    for (const obj of this.tickingObjects) {
+      obj.tick(...params);
+    }
+  }
+}
