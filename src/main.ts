c2881d8227b5ef2b88aa763c2b9930d38fd1c67b
LeonBaudouin <leon@agence-belle-epoque.fr>
2021-11-30 09:38:15 +0100

    :wrench: Added prettier and editorconfig

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
-import './style.css'
+import "./style.css";
+import WebGL from "./webgl";
 
-const app = document.querySelector<HTMLDivElement>('#app')!
+const app = document.querySelector<HTMLDivElement>("#app")!;
 
 app.innerHTML = `
   <h1>Hello Vite!</h1>
   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+`;
+
+const canvas = document.querySelector<HTMLCanvasElement>("#webgl")!;
+
+const webgl = new WebGL(canvas);
+
+const raf = () => {
+  webgl.tick();
+  requestAnimationFrame(raf);
+};
+
+raf();
