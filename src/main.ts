a88513e2e267d94f61258715631f69e4f5540756
Léon Baudouin <leondedouin@gmail.com>
2021-12-03 01:22:19 +0100

    :bento: Added sounds and howler package

--- src/main.ts
+++ src/main.ts
@@ -2,18 +2,29 @@ import "./style.css"
 import WebGL from "./webgl"
 import Stats from "stats.js"
 import observableState from "./utils/observableState"
+import { Howl } from "howler"
+import soundUrl from "./assets/sounds/ui/ui.wav"
+
 const canvas = document.querySelector<HTMLCanvasElement>("#webgl")!
 
 type GameStep = "start" | "game"
 
 const globalState = observableState<{ step: GameStep }>({ step: "start" })
+
+const sounds = {
+  ui: new Howl({ src: [soundUrl] }),
+}
+export type Sounds = typeof sounds
 export type GlobalState = typeof globalState
 
 const mainMenu = document.querySelector<HTMLElement>("#mainMenu")!
 const hud = document.querySelector<HTMLElement>("#hud")!
 const startButton = mainMenu.querySelector<HTMLButtonElement>(".button")!
 
-startButton.addEventListener("click", () => (globalState.step = "game"))
+startButton.addEventListener("click", () => {
+  globalState.step = "game"
+  sounds.ui.play()
+})
 
 globalState.__onChange(
   "step",
@@ -28,9 +39,9 @@ setTimeout(() => {
   // "Dev mode"
   globalState.step = "game"
   mainMenu.style.display = "none"
-}, 700);
+}, 700)
 
-const webgl = new WebGL(canvas, globalState)
+const webgl = new WebGL(canvas, globalState, sounds)
 
 const stats = new Stats()
 document.body.appendChild(stats.dom)
