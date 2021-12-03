34ba900c07c325aa2ba964bf20ac31250745549b
Michael <ac2031@hotmail.com>
2021-12-03 12:39:07 +0100

    :construction: Last commit before refacto

---
+++ src/utils/throttle.ts
@@ -0,0 +1,17 @@
+
+/**
+ * @param {Function} func
+ * @param {Number} limit
+ */
+ export function throttle(func: Function, limit: number) {
+  let inThrottle:any;
+  return function () {
+    const args = arguments;
+    const context = this;
+    if (!inThrottle) {
+      func.apply(context, args);
+      inThrottle = true;
+      setTimeout(() => (inThrottle = false), limit);
+    }
+  };
+}
