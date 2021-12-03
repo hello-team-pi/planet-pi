7894d510ddfeea0e66b0f5da0c8ce355cac816dd
Léon Baudouin <leondedouin@gmail.com>
2021-12-03 01:32:51 +0100

    :musical_note: Added ambiant sound

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,8 @@ import WebGL from "./webgl"
 import Stats from "stats.js"
 import observableState from "./utils/observableState"
 import { Howl } from "howler"
-import soundUrl from "./assets/sounds/ui/ui.wav"
+import uiSoundUrl from "./assets/sounds/ui/ui.wav"
+import ambiantSoundUrl from "./assets/sounds/ambiant/espace.wav"
 
 const canvas = document.querySelector<HTMLCanvasElement>("#webgl")!
 
@@ -12,7 +13,8 @@ type GameStep = "start" | "game"
 const globalState = observableState<{ step: GameStep }>({ step: "start" })
 
 const sounds = {
-  ui: new Howl({ src: [soundUrl] }),
+  ui: new Howl({ src: [uiSoundUrl] }),
+  ambiant: new Howl({ src: [ambiantSoundUrl], loop: true, volume: 0.2 }),
 }
 export type Sounds = typeof sounds
 export type GlobalState = typeof globalState
@@ -21,10 +23,12 @@ const mainMenu = document.querySelector<HTMLElement>("#mainMenu")!
 const hud = document.querySelector<HTMLElement>("#hud")!
 const startButton = mainMenu.querySelector<HTMLButtonElement>(".button")!
 
-startButton.addEventListener("click", () => {
+const start = () => {
   globalState.step = "game"
   sounds.ui.play()
-})
+  sounds.ambiant.play()
+}
+startButton.addEventListener("click", start)
 
 globalState.__onChange(
   "step",
@@ -35,11 +39,8 @@ globalState.__onChange(
   true,
 )
 
-setTimeout(() => {
-  // "Dev mode"
-  globalState.step = "game"
-  mainMenu.style.display = "none"
-}, 700)
+// "Dev mode"
+setTimeout(start, 700)
 
 const webgl = new WebGL(canvas, globalState, sounds)
 
