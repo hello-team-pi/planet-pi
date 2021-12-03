e4e7bcc883e5fb050374f869b0b6efc87e7433a2
LeonBaudouin <leon@agence-belle-epoque.fr>
2021-12-03 11:59:32 +0100

    :lipstick:  Added  cursor to grabobject

--- src/webgl/Components/GrabObject/index.ts
+++ src/webgl/Components/GrabObject/index.ts
@@ -1,23 +1,36 @@
-import { Mesh, MeshBasicMaterial, Object3D, PlaneBufferGeometry, Quaternion, TextureLoader, Vector3 } from "three";
-import { MainSceneContext } from "../../Scenes/MainScene";
-import CursorController from "../People/CursorController";
-import PhysicsObject from "../PhysicsObject";
-import Planet from "../Planet";
-import peopleImage from "../../../assets/images/placeholder.png"
-import { fromPolarX, fromPolarY } from "../../../utils/math/fromPolar";
-import PeopleController from "../People/PeopleController";
-import tuple from "../../../utils/types/tuple";
-import PhysicsController from "../People/PhysicsController";
-import getViewport, { Viewport } from "../../../utils/webgl/viewport";
+import {
+  Mesh,
+  MeshBasicMaterial,
+  Object3D,
+  PlaneBufferGeometry,
+  Quaternion,
+  TextureLoader,
+  Vector3,
+} from "three"
+import { MainSceneContext } from "../../Scenes/MainScene"
+import CursorController from "../People/CursorController"
+import PhysicsObject from "../PhysicsObject"
+import Planet from "../Planet"
+import cursorImage from "../../../assets/images/ui/cursor_target.png"
+import { fromPolarX, fromPolarY } from "../../../utils/math/fromPolar"
+import PeopleController from "../People/PeopleController"
+import tuple from "../../../utils/types/tuple"
+import PhysicsController from "../People/PhysicsController"
+import getViewport, { Viewport } from "../../../utils/webgl/viewport"
 
-export type OnLanding = (previousPlanet: Planet, landedPlanet: Planet, physicsController: PhysicsController, grabObject: GrabObject) => void
+export type OnLanding = (
+  previousPlanet: Planet,
+  landedPlanet: Planet,
+  physicsController: PhysicsController,
+  grabObject: GrabObject,
+) => void
 export type OnDeath = (physicsController: PhysicsController) => void
 
 const temporaryVectors = {
   gravity: new Vector3(),
   target: new Vector3(),
   oppositePosition: new Vector3(),
-  lerpedTargetRotation: new Vector3()
+  lerpedTargetRotation: new Vector3(),
 }
 
 export default class GrabObject extends PhysicsObject {
@@ -28,10 +41,17 @@ export default class GrabObject extends PhysicsObject {
   private onLanding: OnLanding
   private onDeath: OnDeath
   private hasDisappeared = false
-  private viewport : Viewport
+  private viewport: Viewport
   private offsetRadius = 1.2
+  private icon: THREE.Mesh<THREE.BufferGeometry, THREE.MeshBasicMaterial>
 
-  constructor(context: MainSceneContext, originPlanet: Planet, mass = 1, onLanding: OnLanding, onDeath: OnDeath ) {
+  constructor(
+    context: MainSceneContext,
+    originPlanet: Planet,
+    mass = 1,
+    onLanding: OnLanding,
+    onDeath: OnDeath,
+  ) {
     super(mass)
 
     this.cursor = new CursorController(context)
@@ -39,38 +59,69 @@ export default class GrabObject extends PhysicsObject {
     this.cursor.setCurrentPlanet(this.currentPlanet)
     this.onLanding = onLanding
     this.onDeath = onDeath
-    // this.output = new Mesh(new PlaneBufferGeometry(), new MeshBasicMaterial({ map: new TextureLoader().load(peopleImage) }))
+    this.icon = new Mesh(
+      new PlaneBufferGeometry(0.65, 1),
+      new MeshBasicMaterial({ map: new TextureLoader().load(cursorImage), transparent: true }),
+    )
     this.output = new Object3D()
     this.viewport = getViewport(context.camera)
 
-    this.output.scale.setScalar(0.3)
-    // this.output = new Object3D()
+    this.icon.scale.setScalar(0.6)
+
+    this.output = new Object3D()
+    this.output.add(this.icon)
     this.peopleControllerTuples = []
 
     this.rotateAroundPlanet()
   }
 
   private rotateAroundPlanet(alpha = 0.75) {
-    const angle = -Math.atan2(this.cursor.cursorPos.y - this.currentPlanet.position.y, this.cursor.cursorPos.x - this.currentPlanet.position.x)
+    const angle = -Math.atan2(
+      this.cursor.cursorPos.y - this.currentPlanet.position.y,
+      this.cursor.cursorPos.x - this.currentPlanet.position.x,
+    )
 
     const x = fromPolarX(this.currentPlanet.radius + this.offsetRadius, angle + Math.PI / 2)
     const y = fromPolarY(this.currentPlanet.radius + this.offsetRadius, angle + Math.PI / 2)
 
-    temporaryVectors.lerpedTargetRotation.set(this.currentPlanet.position.x - x, this.currentPlanet.position.y - y, 0)
+    temporaryVectors.lerpedTargetRotation.set(
+      this.currentPlanet.position.x - x,
+      this.currentPlanet.position.y - y,
+      0,
+    )
+    this.output.rotation.z = -angle + Math.PI / 2
+    this.icon.position.y = -(this.offsetRadius + this.currentPlanet.radius) * 2
     this.output.position.lerp(temporaryVectors.lerpedTargetRotation, alpha)
   }
 
-  public setPhysicalPeopleControllers = (peopleControllers: PeopleController[], planets: Planet[]) => {
+  public setPhysicalPeopleControllers = (
+    peopleControllers: PeopleController[],
+    planets: Planet[],
+  ) => {
     for (let index = 0; index < peopleControllers.length; index++) {
-      const peopleController = peopleControllers[index];
-      this.peopleControllerTuples.push(tuple(peopleController, new PhysicsController(index, peopleController, this.currentPlanet, planets, this, this.viewport, this.onLanding, this.onDeath)))
+      const peopleController = peopleControllers[index]
+      this.peopleControllerTuples.push(
+        tuple(
+          peopleController,
+          new PhysicsController(
+            index,
+            peopleController,
+            this.currentPlanet,
+            planets,
+            this,
+            this.viewport,
+            this.onLanding,
+            this.onDeath,
+          ),
+        ),
+      )
     }
   }
 
   public repulsePhysicalPeopleControllers() {
     for (const peopleController of this.peopleControllerTuples) {
       const physicsController = peopleController[1]
-      if(!physicsController) return
+      if (!physicsController) return
       physicsController.setReleasedCursorPosition(this.cursor.cursorPos)
       physicsController.setState("REPULSING")
     }
@@ -85,12 +136,12 @@ export default class GrabObject extends PhysicsObject {
   }
 
   // /TODO: hlep
-  public disappear(){
-    this.output.scale.setScalar(0)
+  public disappear() {
+    this.icon.scale.setScalar(0)
     this.hasDisappeared = true
   }
 
   public tick() {
-    if(!this.hasDisappeared) this.rotateAroundPlanet()
+    if (!this.hasDisappeared) this.rotateAroundPlanet()
   }
 }
